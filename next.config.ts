cdf857847b3a2564ff988f48c47f53538bae4601
Vimu Kale <vimu.kale@mindbowser.com>
2026-02-25 13:04:36 +0530

    Add GitHub Actions workflow for deploying to GitHub Pages and configure Next.js for GitHub Pages support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isGhPages = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isGhPages ? "/fhir-search" : "",
+  assetPrefix: isGhPages ? "/fhir-search/" : undefined,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
